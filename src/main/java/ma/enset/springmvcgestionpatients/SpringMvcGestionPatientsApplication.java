b4aaaf44cc97019b42083895c42facf75d81cec9
Lenovo_Youssouf <ytangara2003@gmail.com>
2025-03-17 09:47:38 +0000

    Initial commit

---
+++ src/main/java/ma/enset/springmvcgestionpatients/SpringMvcGestionPatientsApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.springmvcgestionpatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcGestionPatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcGestionPatientsApplication.class, args);
+	}
+
+}
